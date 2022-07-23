33ac056c521387811b39de7e2079a146b1b8ca82
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-23 20:28:44 +0300

    Custom hooks-custom hooks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoaded(false);
+  //   fetch("http://localhost:4000/posts")
+  //     .then((r) => r.json())
+  //     .then((posts) => {
+  //       setPosts(posts);
+  //       setIsLoaded(true);
+  //     });
+  // }, []);
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+  // useEffect(() => {
+  //   document.title = "Underreacted | Home";
+  // }, []);
 
+  useDocumentTitle("Underreacted | Home");
   return (
     <>
       <About />
--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,6 +1,30 @@
 import { useEffect, useState } from "react";
 
 // take in the url
+// function useQuery() {
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   // rename `posts` to a more generic `data`
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     setIsLoaded(false);
+//     fetch('http://localhost:4000/posts')
+//       .then((r) => r.json())
+//       .then((data) => {
+//         setData(data);
+//         setIsLoaded(true);
+//       });
+//   }, []);
+//   // the url is now a dependency
+//   // we want to use the side effect whenever the url changes
+
+//   // return an *object* with the data and isLoaded state
+//   return {
+//     posts: posts,
+//     isLoaded: isLoaded,
+//   };
+// }
+
 function useQuery(url) {
   const [isLoaded, setIsLoaded] = useState(false);
   // rename `posts` to a more generic `data`
@@ -22,4 +46,6 @@ function useQuery(url) {
   return { data, isLoaded };
 }
 
+
 export default useQuery;
+
